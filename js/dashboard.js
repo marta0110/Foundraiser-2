65de65bf3cc678321e53ea13cf80aa2988a33d35
Marta <marta.grzegowska@gmail.com>
2018-12-21 01:48:58 +0100

    rwd volunteers form

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-let wrapperMenu=document.querySelector('.wrapper-menu');let timesClicked=0;wrapperMenu.onclick=function(){timesClicked++;if(timesClicked%2==0){document.getElementById("mySidenav").style.width="0";wrapperMenu.classList.toggle('open');console.log("close")}else{wrapperMenu.classList.toggle('open');document.getElementById("mySidenav").style.width="50%";console.log("open")}}
-const section1=document.querySelector(".swan");const anchor1=document.querySelector(".sidenav_scroll .observer .dot-obs:nth-child(1)");const section2=document.querySelector(".donation");const anchor2=document.querySelector(".sidenav_scroll .observer .dot-obs:nth-child(2)");const section3=document.querySelector(".events");const anchor3=document.querySelector(".sidenav_scroll .observer .dot-obs:nth-child(3)");const section4=document.querySelector(".section4_loggedin");const anchor4=document.querySelector(".sidenav_scroll .observer .dot-obs:nth-child(4)");const section5=document.querySelector(".volunteer_section");const anchor5=document.querySelector(".sidenav_scroll .observer .dot-obs:nth-child(5)");let observerDailyTasks=new IntersectionObserver(entries=>{entries.forEach(entry=>{if(entry.intersectionRatio>0){anchor1.classList.add("activeAnchor")}else{anchor1.classList.remove("activeAnchor")}})});observerDailyTasks.observe(section1);console.log("observe")
-let section2observer=new IntersectionObserver(entries=>{entries.forEach(entry=>{if(entry.intersectionRatio>0){anchor2.classList.add("activeAnchor")}else{anchor2.classList.remove("activeAnchor")}})});section2observer.observe(section2);let section3observer=new IntersectionObserver(entries=>{entries.forEach(entry=>{if(entry.intersectionRatio>0){anchor3.classList.add("activeAnchor")}else{anchor3.classList.remove("activeAnchor")}})});section3observer.observe(section3);let section4observer=new IntersectionObserver(entries=>{entries.forEach(entry=>{if(entry.intersectionRatio>0){anchor4.classList.add("activeAnchor")}else{anchor4.classList.remove("activeAnchor")}})});section4observer.observe(section4);let section5observer=new IntersectionObserver(entries=>{entries.forEach(entry=>{if(entry.intersectionRatio>0){anchor5.classList.add("activeAnchor")}else{anchor5.classList.remove("activeAnchor")}})});section5observer.observe(section5)
-=======
+
 const endpoint = "https://5bfd717f827c3800139ae958.mockapi.io/donationdatabase";
 
 fetch("https://5bfd717f827c3800139ae958.mockapi.io/donationdatabase")
@@ -100,4 +96,4 @@ fetch("https://5bfd717f827c3800139ae958.mockapi.io/donationdatabase")
 
 
    });
->>>>>>> 7939c9cd78bdad0398037ab8f715494ce768280e
+
