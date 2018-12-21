c48d16fb354dc48772f2725c4be918092673689d
Marta <marta.grzegowska@gmail.com>
2018-12-21 02:20:11 +0100

    volunteer rwd iform

--- js/donation_login.js
+++ js/donation_login.js
@@ -1,1 +1,80 @@
-function MainPage(){window.location="homepage.html"}function GoBack(){window.location="donation_system.html"}$(".button_one_payment").on("click",function(){$(".container_one_payment").slideToggle(),$(".container").slideToggle()}),$(".button_login_payment").on("click",function(){$(".login_system").slideToggle()}),$(".button_signup").on("click",function(){$(".signup_system").slideToggle()}),$(".sign_up_button").on("click",function(){$(".signup_system").slideToggle()}),$(".login_button").on("click",function(){$(".login_system").slideToggle(),$(".signup_system").slideToggle()}),$(".login_button_registration").on("click",function(){$(".login_system").slideToggle(),$(".registration_complete").slideToggle()}),$(".button_signup_volunteer").on("click",function(){$(".signup_system_step1").slideToggle()}),$("#confirm").on("click",function(){$(".signup_system").slideToggle()}),$(".one_type_donation").on("click",function(){$(".donation_complete_notlogedin").slideToggle(),$(".container_one_payment").slideToggle()}),$(".donation_login").on("click",function(){$(".donation_complete_logged_in").slideToggle(),$(".support").css("margin-top","25vh"),$("#container_payment_loggedin").slideToggle()});
+//donation_system one off payment events - 
+
+$(".button_one_payment").on('click', function () {
+	$(".container_one_payment").slideToggle(), "slow";
+	$(".container").slideToggle(), "slow";
+});
+
+$(".button_login_payment").on('click', function () {
+	$(".login_system").slideToggle(), "slow";
+});
+
+$(".button_signup").on('click', function () {
+	$(".signup_system").slideToggle(), "slow";
+});
+
+$(".sign_up_button").on('click', function () {
+	$(".signup_system").slideToggle(), "slow";
+});
+
+$(".login_button").on('click', function () {
+	$(".login_system").slideToggle(), "slow";
+	$(".signup_system").slideToggle(), "slow";
+});
+
+
+$(".login_button_registration").on('click', function () {
+	$(".login_system").slideToggle(), "slow";
+	$(".registration_complete").slideToggle(), "slow";
+});
+
+
+$(".button_signup_volunteer").on('click', function () {
+$(".signup_system_step1").slideToggle(), "slow";
+
+});
+
+$("#confirm").on('click', function () {
+	$(".signup_system").slideToggle(), "slow";
+});
+
+
+
+// DONATE BUTTON WITHOUT LOGIN 
+$(".one_type_donation").on('click', function () {
+$(".donation_complete_notlogedin").slideToggle(), "slow";
+$(".container_one_payment").slideToggle(), "slow";
+});
+
+
+// DONATE BUTTON WITH LOGIN 
+
+$(".donation_login").on('click',function(){
+ $(".donation_complete_logged_in").slideToggle(), "slow";
+	$(".support").css("margin-top", "25vh");
+$("#container_payment_loggedin").slideToggle(), "slow";
+	 
+	
+  });
+
+// VOLUNTEER WITH LOGIN  
+$(".join").on('click', function () {
+	$(".volunteer_form").slideToggle(), "slow";
+
+});
+
+$(".volunteer_loggedin").on('click', function () {
+	$(".volunteer_form_thankyou").slideToggle(), "slow";
+	$(".volunteer_form").slideToggle(), "slow";
+
+});
+
+
+// function - go to the landing page or frontpage
+function MainPage(){
+	 window.location='homepage.html'
+}
+
+function GoBack() {
+	window.location = 'donation_system.html'
+}
